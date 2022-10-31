8a1ca684871003f012290afca609eabcf501f160
samixtures <samibajwa@csu.fullerton.edu>
2022-10-31 16:15:46 -0700

    struggling to import images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css'
+import {bat_vs_freeze} from "./bat_vs_freeze"
+
+const Fight = (prop) => {
+  return (
+    <img src='/bat_vs_freeze/bat_freeze1.1.png' alt = "bat fighting" />
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <h1 className = "title">Cold Days</h1>
+      <img src="bat_vs_freeze/bat_freeze1.1.png" alt = "bat fight"/>
+      <Fight/>
     </div>
   );
 }
